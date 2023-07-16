0f8796b836f3b1254eb829cdd043a23036c8221f
Muhammad Alam <malam53345@gmail.comm>
2023-07-16 15:06:13 +0500

    completed

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -2,17 +2,34 @@ import Image from "next/image";
 import classes from "./navbar.module.css";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { RxCross2 } from "react-icons/rx";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Link from "next/link";
-export default function Navbar() {
-  const [hambarger, setHambarger] = useState(true);
 
-  const hambargerHandler = () => {
-    setHambarger(!hambarger);
+export default function Navbar() {
+  const [hamburger, setHamburger] = useState(true);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 1003) {
+        setHamburger(true);
+      }
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const hamburgerHandler = () => {
+    setHamburger(!hamburger);
   };
 
   return (
-    <div className={classes.mainNav}>
+    <div className={`${classes.mainNav} ${hamburger ? "" : classes.vertical}`}>
       <div className={classes.left}>
         <Link href="#home" scroll={false}>
           <Image
@@ -20,37 +37,74 @@ export default function Navbar() {
             width={200}
             height={40}
             alt="logo"
-            className={classes.img}
+            className={`${hamburger ? classes.img : classes.verticalImg}`}
           />
         </Link>
       </div>
-      <div className={classes.right}>
-        <ul>
-          <li>
+      <div className={`${hamburger ? classes.right : classes.verticalUl}`}>
+        {hamburger ? (
+          <ul>
+            <li>
+              <Link
+                href="#about"
+                scroll={false}
+                style={{
+                  textDecoration: "none",
+                  color: "white",
+                }}
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="#working"
+                scroll={false}
+                style={{
+                  textDecoration: "none",
+                  color: "white",
+                }}
+              >
+                How it Works
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="#stacking"
+                scroll={false}
+                style={{
+                  textDecoration: "none",
+                  color: "white",
+                }}
+              >
+                Stacking
+              </Link>
+            </li>
+          </ul>
+        ) : (
+          <div className={classes.verticalLi}>
             <Link
               href="#about"
               scroll={false}
               style={{
                 textDecoration: "none",
                 color: "white",
+                marginRight: "1rem",
               }}
             >
               About
             </Link>
-          </li>
-          <li>
             <Link
               href="#working"
               scroll={false}
               style={{
                 textDecoration: "none",
                 color: "white",
+                marginRight: "1rem",
               }}
             >
               How it Works
             </Link>
-          </li>
-          <li>
             <Link
               href="#stacking"
               scroll={false}
@@ -61,10 +115,10 @@ export default function Navbar() {
             >
               Stacking
             </Link>
-          </li>
-        </ul>
+          </div>
+        )}
         <div className={classes.outerHam}>
-          <div className={classes.btn}>
+          <div className={`${hamburger ? classes.btn : classes.verticalBtn}`}>
             <Image
               src="/images/phantom.png"
               width={14}
@@ -74,19 +128,16 @@ export default function Navbar() {
             />
             <button>CONNECT PHANTOM WALLET</button>
           </div>
-          {hambarger ? (
-            <GiHamburgerMenu
-              className={classes.hambarger}
-              onClick={hambargerHandler}
-            />
-          ) : (
-            <RxCross2
-              className={classes.hambarger}
-              onClick={hambargerHandler}
-            />
-          )}
         </div>
       </div>
+      {hamburger ? (
+        <GiHamburgerMenu
+          className={classes.hambarger}
+          onClick={hamburgerHandler}
+        />
+      ) : (
+        <RxCross2 className={classes.hambarger} onClick={hamburgerHandler} />
+      )}
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
-
-import Hero from "@/components/hero/hero"
-import Stack from "@/components/hero/stack"
-import Navbar from "@/components/navbar/navbar"
-import StackingPool from "@/components/stacking pool/stacking-pool"
-import Stacking from "@/components/stacking/stacking"
-import Work from "@/components/stacking/work"
+import Hero from "@/components/hero/hero";
+import Stack from "@/components/hero/stack";
+import Navbar from "@/components/navbar/navbar";
+import StackingPool from "@/components/stacking pool/stacking-pool";
+import Stacking from "@/components/stacking/stacking";
+import Work from "@/components/stacking/work";
+import Head from "next/head";
 export default function HomePage() {
   return (
     <div>
+      <Head>
+        <title>Allovr</title>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
       <Navbar />
       <Hero />
       <Stack />
@@ -15,5 +19,5 @@ export default function HomePage() {
       <Work />
       <StackingPool />
     </div>
-  )
+  );
 }
